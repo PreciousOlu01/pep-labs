8423de880630d1161e987b21aa00025e7ca77a51
PreciousOlu01 <prcsolusegun@gmail.com>
2022-12-20 04:06:25 +0000

    Saved progress on MethodParameters 2022-12-20 04:06:25 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -11,7 +11,11 @@ public class Parrot {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
-    public String copy(String text){
-        return "return text please!";
+    public static String copy(String text){
+        return text;
     }
+    public static void main(String[] args){
+        String copyString =copy("hello");
+        System.out.println(copyString);
+    } 
 }
