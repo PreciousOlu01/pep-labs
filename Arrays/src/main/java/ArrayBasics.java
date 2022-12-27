a0bb9d24e4e09062dccb017775f28ca74b37adf5
PreciousOlu01 <prcsolusegun@gmail.com>
2022-12-27 20:40:55 +0000

    Saved progress on Arrays 2022-12-27 20:40:55 +0000

--- Arrays/src/main/java/ArrayBasics.java
+++ Arrays/src/main/java/ArrayBasics.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * An array is a group of some predefined number of elements.
@@ -18,7 +19,6 @@ public class ArrayBasics {
     public int getLengthOfArray(int[] arr){
         int arrLength=arr.length;
         for(int i=0; i<arrLength; i++){
-            System.out.println(" "+ arr[i]);
             count++;
         }
         return count;
@@ -34,10 +34,12 @@ public class ArrayBasics {
      */
     public int getNthElementOfArray(int[] arr, int n){
         int len= arr.length;
+        int[] newArray = new int[len + 1];
         for(int i=0; i<len; i++){
-            System.out.println(arr[i]);
+            newArray[i]=arr[i];
         }
-        return arr[n];
+        return newArray[n];
+        
     }
 
     /**
@@ -49,13 +51,20 @@ public class ArrayBasics {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-        int[] newArray= new int[n+1];
-        for(int i=0; i<n; i++){
-            newArray[i]=arr[i];
-            newArray[n]=val;
+        int len= arr.length;
+        int[] newArray= new int[len+1];
+        int j=0;
+        for(int i=0; i<newArray.length-1; i++){
+            if(n==arr[i]){
+                newArray[j]=val;
+            }
+            else{
+                newArray[j]=arr[i];
+                j++;
+            }    
         }
-        System.out.println(newArray[n]);
-
+        newArray[n]=val;
+        System.out.println(Arrays.toString(newArray));
     }
 
     /**
@@ -65,5 +74,21 @@ public class ArrayBasics {
     public int[] returnNewArraySizeN(int n){
         int[] arrSize = new int[n];
         return arrSize;
+    }
+
+    public static void main(String[] args){
+        ArrayBasics arrays = new ArrayBasics();
+
+        int[] arr= {2,4,6};
+        int indexNum=2;
+        int val = 8;
+
+        int result = arrays.getLengthOfArray(arr);
+        int results = arrays.getNthElementOfArray(arr, indexNum);
+        arrays.setNthElementOfArray(arr, indexNum, val);
+
+        System.out.println(result);
+        System.out.println(results);
+        
     }
 }
