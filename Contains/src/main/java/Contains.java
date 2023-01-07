de5a58ab50350e7c663b06bd48e9a6667499cc7a
PreciousOlu01 <prcsolusegun@gmail.com>
2023-01-07 23:12:17 +0000

    Saved progress on Contains 2023-01-07 23:12:17 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -11,6 +11,12 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        boolean ifContain=true;
+        for(int i=0; i<arr.length; i++){
+            if(target==arr[i]){
+                return ifContain;
+            }
+        }
         return false;
     }
 }
