66f11d4aa74ed322b4557916225678ac66b7ae64
PreciousOlu01 <prcsolusegun@gmail.com>
2023-01-17 02:02:43 +0000

    Saved progress on InnerJoin 2023-01-17 02:02:43 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post(id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES user(id)VALUES(1,"post",1));
+CREATE TABLE post(id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES user(id)VALUES('post1',12));
