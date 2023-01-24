adc88db857b020a7b54ee37af5f35d4fd373656d
PreciousOlu01 <prcsolusegun@gmail.com>
2023-01-24 05:14:01 +0000

    Saved progress on Pyramid 2023-01-24 05:14:01 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,19 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
         return "";
+    }
+
+    public static void main(String[] args){
+        Pyramid obj= new Pyramid();
+        int x= 5;
+
+        System.out.println(obj.returnPyramid(x));
     }
 }
