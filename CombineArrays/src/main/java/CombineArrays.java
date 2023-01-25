cf2349f566bc9514901fc5d1296c1699bb17f487
PreciousOlu01 <prcsolusegun@gmail.com>
2023-01-25 17:00:13 +0000

    Saved progress on CombineArrays 2023-01-25 17:00:13 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,17 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int a= arr1.length;
+        int b= arr2.length;
+        int c= a + b;
+        int[] arr3 = new int[c];
 
-        return null;
+        for(int i=0; i<a; i++){
+            arr3[i]= arr1[i];
+        }
+        for(int j=0; j<b; j++){
+            arr3[a + j]= arr2[j];
+        }
+        return arr3;
     }
 }
